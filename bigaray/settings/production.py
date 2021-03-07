6b51aec7ae0777fe54b7b069f822b2fd59b40de2
webdev0101 <hesimiao2019@gmail.com>
2021-03-07 22:28:00 +0800

    removed ssl

--- bigaray/settings/production.py
+++ bigaray/settings/production.py
@@ -20,11 +20,3 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
-
-SECURE_SSL_REDIRECT = True
-
-SESSION_COOKIE_SECURE = True
-
-CSRF_COOKIE_SECURE = True
-
-SECURE_BROWSER_XSS_FILTER = True
