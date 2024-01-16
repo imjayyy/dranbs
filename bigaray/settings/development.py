150b622f4ea951bfc5241f93e66139be79874a44
Mujtaba <mujtabajafri3@gmail.com>
2024-01-16 11:45:34 +0500

    deployment 1

--- bigaray/settings/development.py
+++ bigaray/settings/development.py
@@ -12,13 +12,23 @@ ALLOWED_HOSTS = [
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'deployphoenix_prod',
+#         'USER': 'deployphoenix',
+#         'PASSWORD': '#;rC$3AJHC9~xKh<',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432'
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'deployphoenix_prod',
-        'USER': 'deployphoenix',
-        'PASSWORD': '#;rC$3AJHC9~xKh<',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.environ.get('DB_DATABASE', os.path.join(BASE_DIR, 'postgres')),
+        'USER': os.environ.get('DB_USER', 'postgres'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
