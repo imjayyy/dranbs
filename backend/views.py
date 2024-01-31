8b10d8df85bf2e2fc44f66804fab3c885eb089c3
Mujtaba <mujtabajafri3@gmail.com>
2024-02-01 00:36:34 +0500

    image_test

--- backend/views.py
+++ backend/views.py
@@ -809,6 +809,10 @@ def get_total_new_count(request):
 class ImageView(View):
     def get(self, request, subdir, filename):
         directory = '/var/www/backend/dranbs/images/'
+        with open("/var/www/backend/dranbs/images/{0}/{1}".format(subdir, filename), "rb") as f:
+                mime = mimetypes.MimeTypes().guess_type("/var/www/backend/dranbs/images/{0}/{1}".format(subdir, filename))[0]
+                response = HttpResponse(f.read(), content_type=mime)
+                return response
         try:
             with open("/var/www/backend/dranbs/images/{0}/{1}".format(subdir, filename), "rb") as f:
                 mime = mimetypes.MimeTypes().guess_type("/var/www/backend/dranbs/images/{0}/{1}".format(subdir, filename))[0]
